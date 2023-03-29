5790de8aa138596c80b7451ee4f939e1057b25db
Patrick M <patrickmatias.silva@hotmail.com>
2023-03-29 15:18:04 -0300

    feat: project fonts configuration

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,6 +15,9 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;600;700;800&family=Poppins:wght@400;500;600;700;800" rel="stylesheet" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      title: ["Poppins", ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-};
+}
